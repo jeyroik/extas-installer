7c3f0ba41e08bee82be682debdae35bf121d5c7f
jeyroik <jeyroik@gmail.com>
2020-03-04 17:35:37 +0300

    added exporter test

---
+++ tests/packages/ExporterTest.php
@@ -0,0 +1,59 @@
+<?php
+
+use \PHPUnit\Framework\TestCase;
+use \extas\components\plugins\Plugin;
+use extas\components\plugins\PluginRepository;
+use \extas\interfaces\repositories\IRepository;
+
+/**
+ * Class ExporterTest
+ *
+ * @author jeyroik <jeyroik@gmail.com>
+ */
+class ExporterTest extends TestCase
+{
+    /**
+     * @var IRepository|null
+     */
+    protected ?IRepository $pluginRepo = null;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $env = \Dotenv\Dotenv::create(getcwd() . '/tests/');
+        $env->load();
+        $this->pluginRepo = new PluginRepository();
+    }
+
+    /**
+     * Clean up
+     */
+    public function tearDown(): void
+    {
+        $this->pluginRepo->delete([Plugin::FIELD__CLASS => 'NotExistingClass']);
+    }
+
+    public function testExportEntities()
+    {
+        $this->pluginRepo->create(new Plugin([
+            Plugin::FIELD__CLASS => 'NotExistingClass',
+            Plugin::FIELD__STAGE => 'extas.export.test'
+        ]));
+
+        $exporter = new \extas\components\packages\Exporter();
+        $this->expectExceptionMessage('Unknown class "NotExistingClass"');
+        $exporter->export(['test']);
+    }
+
+    public function testExport()
+    {
+        $this->pluginRepo->create(new Plugin([
+            Plugin::FIELD__CLASS => 'NotExistingClass',
+            Plugin::FIELD__STAGE => 'extas.export'
+        ]));
+
+        $exporter = new \extas\components\packages\Exporter();
+        $this->expectExceptionMessage('Unknown class "NotExistingClass"');
+        $exporter->export();
+    }
+}
